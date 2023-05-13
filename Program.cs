41e5044f55363ae2eccc8f038bf0b79a13c5fa7a
colatti2017@gmail.com <e107ti@etec.sp.gov.br>
2023-05-12 22:19:08 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(" ----FluxogramaDaEngenharia----/n ");
+
+Console.Write("Esta se Movendo (S/N)? ");
+string Movendo = Console.ReadLine()!;
+
+if (Movendo == "S")
+{
+    Console.Write("Deveria (S/N)  ");
+    string Deveria = Console.ReadLine()!;
+
+    if (Deveria == "S") {
+        Console.WriteLine("Ótimo");
+    }else if (Deveria == "N") {
+        Console.WriteLine("Passe silver tape ");
+    } else {
+        Console.WriteLine("Opção invalida !");
+    }
+}
+else if (Movendo == "N"){
+     Console.Write("Deveria (S/N)  ");
+    string Deveria = Console.ReadLine()!;
+    
+    if (Deveria == "S"){
+        Console.WriteLine("Passe WD-40. ");
+    }else if (Deveria == "N"){
+        Console.WriteLine("Ótimo");
+    }else{
+        Console.WriteLine("Opção invalida ");
+    }}else{
+        Console.WriteLine("Opção invalida ");
+}
+
+
+
+
+
+
